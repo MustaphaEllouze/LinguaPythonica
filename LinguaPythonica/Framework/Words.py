7757e1c442b045e79a56aa0d1ffc82a9db154f72
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-05-08 16:45:33 +0200

    rajout de commetnaire et de check rules

--- LinguaPythonica/Framework/Words.py
+++ LinguaPythonica/Framework/Words.py
@@ -37,6 +37,7 @@ class Word(ABC):
                 translation             :str        =   '',
                 add_dictionnary         :bool       =   True,
                 extend_variations       :Word       =   None,
+                check_rules             :bool       =   True,
             ):
         """
         Args:
@@ -46,7 +47,13 @@ class Word(ABC):
             extend_variations (Word, optional): Le mot dont la nouvelle instance est un dérivé, add_dictionnary doti valoir True. Defaults to None.
             check_rules (bool, optional): True s'il faut checker la morphosyntaxique. Defaults to True.
         """
-        
+        # Check si le mot obéit à la morphosyntaxique
+        if check_rules :
+            if self.check_rules():
+                pass
+            else:
+                raise Exception('Construction non permise')
+
         # --- Lettres du mots
         self.letters = letters
 
@@ -94,9 +101,11 @@ class Word(ABC):
     def check_rules(self):
         """Vérifie que les mots obéissent à la morphosyntaxique
         """
-        pass
+        return True
     
 class Root(Word,ABC):
+    """Représente une racine d'un mot (pas de sens particulier)
+    """
     
     # Dictionnaire des mots
     dict_root = {}     # Clés = Str ; Value = self 
@@ -105,17 +114,24 @@ class Root(Word,ABC):
                 self,
                 letters                 :list[Sound],
                 translation             :str        =   '',
-                
             ):
+        """
+        Args:
+            letters (list[str]): Lettres qui composent le mot
+            translation (str, optional): Traduction du mot. Defaults to None.
+        """
+        # Rajouté directement au dictionnaire et est à la base de dérivés, mais ne dérive de rien
         super().__init__(
             letters=letters,
             translation=translation,
             add_dictionnary=True,
             extend_variations=None,
+            check_rules=False,
         )
         Root.dict_root[tuple(letters)]=self
 
 class DerivableWord(Word,ABC):
+    """Représente un mot dérivé"""
     
     def __init__(
             self,
@@ -127,7 +143,8 @@ class DerivableWord(Word,ABC):
             letters=letters,
             translation=translation,
             add_dictionnary=False,
-            extend_variations=extend_variations
+            extend_variations=extend_variations,
+            check_rules=True,
             )
 
     @abstractmethod
