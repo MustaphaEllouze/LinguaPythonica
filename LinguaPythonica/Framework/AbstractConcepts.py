635d60e3e51ec3f2024b0488737903ace2bd70b5
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-05-08 17:07:07 +0200

    Update AbstractConcepts.py

--- LinguaPythonica/Framework/AbstractConcepts.py
+++ LinguaPythonica/Framework/AbstractConcepts.py
@@ -1,7 +1,21 @@
 from abc import ABC,abstractmethod
 
 class Gender(ABC):
-    pass
+    """Représente un genre
+    """
+
+    @abstractmethod
+    def __init__(self):
+        pass
 
 class Person(ABC):
-    pass
+    """Représente le type personne
+    """
+    def __init__(
+            self,
+            type_person:int,
+    ):
+        if type_person not in [1,2,3,4]:
+            raise Exception('Mauvaise type de personne')
+        
+        self.type_person = type_person
