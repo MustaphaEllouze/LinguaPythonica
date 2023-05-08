2353827b28bda02a72a8876732a9c13367ee7d3b
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-05-08 12:18:25 +0200

    tests laspaks

--- tests/LinguaPythonicaTests.py
+++ tests/LinguaPythonicaTests.py
@@ -16,18 +16,24 @@ class TestLaspak(unittest.TestCase):
     ):
         """Vérifie si toutes les consonnes sont définies
         """
-        expected_consonants = []
+        expected_consonants = ['m', 'p', 'b', 't', 'd', 'f', 'v', 'th', 'n', 's', 'z', 'ch', 'j', 'y', 'r', 'l', 'w', 'k', 'g', 'q', 'x', 'h', "'"]
         for cons_symbol in expected_consonants :
-            self.assertIn(cons_symbol,LaspakSounds.CONS.keys())
+            self.assertIn(
+                cons_symbol,
+                LaspakSounds.LaspakConsonant.laspak_consonants.keys()
+                )
     
     def test_01_voyelles(
             self,
     ):
         """Vérifie si toutes les voyelles sont définies
         """
-        expected_vowels = []
+        expected_vowels = ['a', 'e', 'i', 'o', 'u']
         for vow_symbol in expected_vowels :
-            self.assertIn(vow_symbol,LaspakSounds.VOWE.keys())
+            self.assertIn(
+                vow_symbol,
+                LaspakSounds.LaspakVowel.laspak_vowels.keys()
+                )
 
 if __name__ == '__main__':
     unittest.main()
