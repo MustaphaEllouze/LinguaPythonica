59abcb4f40d1e1ee137812010c9c0c7551b34c90
MustaphaEllouze <ellouze.mostafa@gmail.com>
2023-05-08 14:57:56 +0200

    restructuration de Words

--- LinguaPythonica/Framework/Words.py
+++ LinguaPythonica/Framework/Words.py
@@ -1,132 +1,142 @@
-from LinguaPythonica.RedDjinn.sounds import (
-    CONS,
-    VOWE,
-)
+from __future__ import annotations
 
-from LinguaPythonica.Framework.Sounds import (
-    Vowel,
-    Consonant,
+from abc import (
+    ABC,
+    abstractmethod,
 )
 
 from collections import defaultdict
 
-class Word :
-    words = []
-    translations_str = defaultdict(list)
+from LinguaPythonica.Framework.Sounds import Sound
+
+class Word(ABC):
+    """Représente un mot. Classe abstraite.
+
+    Class Attributes : 
+        - dict_words        : dict{str:Word}         : Clés = symboles
+        - translations      : dict{str:list[Word]}   : Clés = traductions
+
+    Attributes : 
+        - letters           :   list[Sound]     : Liste des lettres qui composent le mot
+        - translation       :   str             : Traduction du mot
+        - symbols           :   list[str]       : Liste des symboles qui forment le mot
+        - variations        :   list[Word]      : Liste de variations des mots
+
+    """
+
+    # Dictionnaire des mots
+    dict_words = {}     # Clés = Str ; Value = self 
+
+    # Dictionnaire : str(traduction) --> self
     translations = defaultdict(list)
 
-    def __init__(self,
-                letters,
-                translation=None,
-                add_dictionnary = True,
-                extend_variations = None,
-                ):
-        self.letters = []
-        for l in letters:
-            if(l in VOWE.keys()):
-                self.letters.append(VOWE[l])
-            elif(l in CONS.keys()):
-                self.letters.append(CONS[l])
-            else:
-                raise Exception('Lettre inconnue : '+str(l.symbol))
+    def __init__(
+                self,
+                letters                 :list[Sound],
+                translation             :str        =   '',
+                add_dictionnary         :bool       =   True,
+                extend_variations       :Word       =   None,
+            ):
+        """
+        Args:
+            letters (list[str]): Lettres qui composent le mot
+            translation (str, optional): Traduction du mot. Defaults to None.
+            add_dictionnary (bool, optional): True si le mot doit être . Defaults to True.
+            extend_variations (Word, optional): Le mot dont la nouvelle instance est un dérivé, add_dictionnary doti valoir True. Defaults to None.
+            check_rules (bool, optional): True s'il faut checker la morphosyntaxique. Defaults to True.
+        """
+        
+        # --- Lettres du mots
+        self.letters = letters
+
+        # --- Traduction
         self.translation = translation
-        self.is_verb = False
-        self.is_copula = False
-        self.is_pronoun = False
+
+        # Symboles de chaque lettre du mot
         self.symbols = [let.symbol for let in self.letters]
-        if(add_dictionnary) : Word.words.append(self)
-        self.variations = [self]
-        if(not extend_variations is None):
-            extend_variations.variations.append(self)
-        Word.translations_str[translation].append(str(self))
+
+        # S'il faut ajouter au dictionnaire
+        if(add_dictionnary) : 
+            Word.words[str(self)]=self
+            self.variations = [self]
+
+            # Si le mot est une variation    
+            if(not extend_variations is None):
+                extend_variations.variations.append(self)
+        else:
+            self.variations = None
+        
+        # Ajout dans le dictionnaire de traduction
         Word.translations[translation].append(self)
     
-    def __str__(self):
+    def __str__(self)->str:
         res = ''
         for s in [let.symbol for let in self.letters] : res += s
         return res
-    
-    def plural(self):
-        for i,let in enumerate(self.letters[::-1]):
-            if(type(let) is Vowel):
-                break
-        
-        
-        if(let.symbol == 'a'):
-            letter_to_insert = 'e'
-        elif(let.symbol == 'é'):
-            letter_to_insert = 'i'
-        elif(let.symbol == 'i'):
-            letter_to_insert = 'i'
-        elif(let.symbol == 'o'):
-            letter_to_insert = 'u'
-        elif(let.symbol == 'u'):
-            letter_to_insert = 'u'
-        elif(let.symbol == 'e'):
-            letter_to_insert = 'i'
+
+    def get_word_from_translation(translation)->Word:
+        """Renvoie un mot à partir d'une traduction
+
+        Args:
+            translation (str): Mot dont on veut avoir la traduction
+
+        Returns:
+            Word: Mot traduit ; renvoie None si traduction pas trouvée
+        """
+        if translation in Word.translations.keys():
+            return Word.translations[translation][0]
         else:
-            raise Exception('Vowel non reconnue, ou pas de Vowel')
+            return None
 
-        resultat = self.symbols.copy()
-        resultat.insert(len(self.letters)-i,letter_to_insert)
+    @abstractmethod
+    def plural(self):
+        """Renvoie le pluriel du mot
+        """
+        pass
 
-        return Word(
-            resultat,
-            translation=self.translation+'.PLU',
-            add_dictionnary=False,
-            extend_variations = self,
-        )
-    
-    def masculine(self):
-        return Word(
-            self.symbols+['e'],
-            translation=self.translation+'.MASC',
-            add_dictionnary=False,
-            extend_variations = self,
-        )
-    
-    def genetive_abs(self):
-        return Word(
-            self.symbols+list('pfoe'),
-            translation=self.translation+'.GEN-ABS',
-            add_dictionnary=False,
-            extend_variations = self,
-        )
-    
-    def genetive_erg(self):
-        return Word(
-            self.symbols+list('kral'),
-            translation=self.translation+'.GEN-ERG',
-            add_dictionnary=False,
-            extend_variations = self,
-        )
-    
-    def substantive (self):
-        return Word(
-            self.symbols+['ch','o','s'],
-            translation=self.translation+'.SUBS',
-            add_dictionnary=False,
-            extend_variations = self,
-        )
+    @abstractmethod
+    def substantive(self):
+        """Renvoie le substantif du mot
+        """
+        pass
 
-    def get_word_from_tr(translation):
-        return Word.translations[translation][0]
-    
+    @abstractmethod
+    def check_rules(self):
+        """Vérifie que les mots obéissent à la morphosyntaxique
+        """
+        pass
+            
 
-class Verb(Word):
-    verbs = []
+class Verb(Word,ABC):
+    """Représente un verbe, classe abstraite"""
+
+    # Dictionnaire des mots
+    dict_verbs = {}     # Clés = Str ; Value = self 
+
     def __init__(self,
-                letters,
-                translation=None,
-                add_dictionnary=True,
-                extend_variations = None,
-                check_rules=True,
+                letters             :       list[str]        ,
+                translation         :       str         =  '',
+                add_dictionnary     :       bool        =True,
+                extend_variations   :       Word        =None,
+                check_rules         :       bool        =True,
                 ):
-        super().__init__(letters, translation=translation, add_dictionnary=add_dictionnary,extend_variations=extend_variations)
-        if(check_rules) : self.check_rules()
-        self.is_verb = True
-        Verb.verbs.append(self)
+        """
+        Args:
+            letters (list[str]): Lettres qui composent le mot
+            translation (str, optional): Traduction du mot. Defaults to None.
+            add_dictionnary (bool, optional): True si le mot doit être . Defaults to True.
+            extend_variations (Word, optional): Le mot dont la nouvelle instance est un dérivé, add_dictionnary doti valoir True. Defaults to None.
+            check_rules (bool, optional): True s'il faut checker la morphosyntaxique. Defaults to True.
+        """
+        super().__init__(
+            letters=letters,
+            translation=translation,
+            add_dictionnary=add_dictionnary,
+            extend_variations=extend_variations,
+            check_rules=check_rules,
+        )
     
+    @abstractmethod
     def conjugate(self,
                  tense,
                  name_tense):
@@ -144,14 +154,3 @@ class Verb(Word):
             for pronoun in tense.keys()
             }
         return res
-        
-    def check_rules(self):
-        if(self.letters[-1].liquid or self.letters[-1].nasal):
-            if(self.letters[-2].symbol !='a'):
-                raise Exception('Mauvaise terminaison verbe : '+str(self))
-        if(self.letters[-1].fric):
-            if(self.letters[-2].symbol !='i'):
-                raise Exception('Mauvaise terminaison verbe : '+str(self))
-        if(self.letters[-1].stop):
-            if(self.letters[-2].symbol !='o'):
-                raise Exception('Mauvaise terminaison verbe : '+str(self))
